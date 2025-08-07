a5a9a8dc627135e9a961b94d351f081ea21b61e3
Martin Jochum <mjochum64@gmail.com>
2025-08-07 12:05:24 +0200

    🔖 Checkpoint: Edit /home/mjochum/projekte/KI_TESTS/law-crawler-service/src/main/java/de/legal/crawler/LawCrawlerServiceApplication.java

--- src/main/java/de/legal/crawler/LawCrawlerServiceApplication.java
+++ src/main/java/de/legal/crawler/LawCrawlerServiceApplication.java
@@ -2,23 +2,13 @@ package de.legal.crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Main application class for Legal Document Crawler Service
  * Crawls legal documents from rechtsprechung-im-internet.de
  */
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class,
-    JpaRepositoriesAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableScheduling
 public class LawCrawlerServiceApplication {
 
