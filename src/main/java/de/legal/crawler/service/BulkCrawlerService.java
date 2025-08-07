0d2499370116ac833b03e375d67804fc61ff4838
Martin Jochum <mjochum64@gmail.com>
2025-08-07 15:58:04 +0200

    🔖 Checkpoint: Edit /home/mjochum/projekte/KI_TESTS/law-crawler-service/src/main/java/de/legal/crawler/service/BulkCrawlerService.java

--- src/main/java/de/legal/crawler/service/BulkCrawlerService.java
+++ src/main/java/de/legal/crawler/service/BulkCrawlerService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
