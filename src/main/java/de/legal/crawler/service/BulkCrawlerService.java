2f9fb6089d7af8bc56a3fe63fccfac6b6b78a20a
Martin Jochum <mjochum64@gmail.com>
2025-08-07 15:43:09 +0200

    🔖 Checkpoint: Edit /home/mjochum/projekte/KI_TESTS/law-crawler-service/src/main/java/de/legal/crawler/service/BulkCrawlerService.java

--- src/main/java/de/legal/crawler/service/BulkCrawlerService.java
+++ src/main/java/de/legal/crawler/service/BulkCrawlerService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
